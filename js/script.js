a94538e757f44103250eca2cc3b22e4e3d32a530
Joel Alves <166664754+JoelAlvess@users.noreply.github.com>
2024-10-31 11:05:42 -0300

    refatoração e desenvolvimento
    Ajustei e comecei a comentar algumas partes e desenvolvi a parte de ofertas do dia

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Hero-carousel
 const track = document.querySelector('.carousel-track')
 const images = document.querySelectorAll('.carousel-track img')
 let index = 0
@@ -8,3 +9,15 @@ function moveCarousel (){
 }
 
 setInterval(moveCarousel, 6000)
+
+//shop carousel
+const trackShop = document.querySelector(".track-offer");
+const productShop = document.querySelectorAll(".product");
+let indexShop = 0;
+
+function moveCarouselShop() {
+    indexShop = (indexShop + 1) % productShop.length;
+    trackShop.style.transform = `translateX(-${indexShop * 100}%)`;
+}
+
+setInterval(moveCarouselShop, 6000);
